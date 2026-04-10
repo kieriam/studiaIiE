57ecc783bf5f1295e444228a73e0106627d49170
kieriam <kieriamm@gmail.com>
2026-04-10 19:50:53 +0200

    Podstawowa wersja

--- python01/IleToMa.py
+++ python01/IleToMa.py
@@ -3,26 +3,14 @@ slownik =input()
 print(len(slownik))
 
 i = 0
-stack = []
 
 for x in slownik:
-    if x != " ":
+    if x == " " :
         i = i + 1
-    
-    if x not in stack:
-        stack.append(x)
 
-print("liczba liter : ")
+print("liczba spacji: ")
 print(i)
 
-spacje = slownik.count(" ")
-print("liczba spacji : ")
-print(spacje)
-
-wyrazy = len(slownik.split())
-print("liczba wyrazów : ")
-print(wyrazy)
-
-print("czestotliwosc zanków: ")
-for x in stack:
-    print(str(slownik.count(x)) + x, end = " ")
+i = i + 1
+print("liczba wyrazów: ")
+print(i)
