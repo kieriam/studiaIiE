42c4625f3b349e345501649b5bcb3535ce4d85c5
kieriam <kieriamm@gmail.com>
2026-05-18 20:06:22 +0200

    mv dir python01

---
+++ python/python01/LoveUEP.py
@@ -0,0 +1,14 @@
+NIU = 103106
+for i in range(1,101):
+        if(i == 50):
+            print(NIU)
+        elif(i % 3 == 0):
+            if(i % 5 ==0):
+                print("LoveUEP")
+            else:
+                print("Love")
+        elif(i % 5 == 0):
+            print("UEP")
+        else:
+            print(i)
+                                                        
---
+++ python/python01/NajwiekszaINajmniejsza.py
@@ -0,0 +1,5 @@
+#NiU 103106
+cyfry = [1,0,3,6]
+cyfry.sort()
+print(cyfry[0])
+print(cyfry[len(cyfry)-1])
